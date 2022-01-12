9d905bcc0b635d17265c9dcd8ca3793a306af34d
Sakari Angervuori <sakari.angervuori@cgi.com>
2022-01-12 18:50:26 +0200

    refaktorointia

--- common/Common.js
+++ common/Common.js
@@ -1,5 +1,3 @@
-function common() {
-
     var MathNew = {
         deg2rad: function (deg) {
             return deg * (Math.PI) / 180;
@@ -50,55 +48,6 @@ function common() {
             return rnd;
         },
     };
-}
-
-// Set background color based on UVI index
-function setUviColor(uvIndex) {
-    if (uvIndex >= 10) {
-        setBgColorById("rgb(153,140,255)");
-        setFontColorById("PYSY POIS AURINGOSTA!", "Red");
-    }
-    if (uvIndex < 10 & uvIndex >= 9.0) {
-        setBgColorById("rgb(181,76,255)");
-        setFontColorById("PYSY POIS AURINGOSTA!", "Red");
-    }
-    if (uvIndex >= 8.0 & uvIndex < 9.0) {
-        setBgColorById("rgb(255,0,153)");
-        setFontColorById("PYSY POIS AURINGOSTA!", "Red");
-    }
-    if (uvIndex >= 7.0 & uvIndex < 8.0) {
-        setBgColorById("rgb(216,0,29)");
-        setFontColorById("5 min MAX AURINGOSSA!", "Red");
-    }
-    if (uvIndex >= 6.0 & uvIndex < 7.0) {
-        setBgColorById("rgb(232,44,14)");
-        setFontColorById("15 min MAX AURINGOSSA!", "Red");
-    }
-    if (uvIndex >= 5.0 & uvIndex < 6.0) {
-        setBgColorById("rgb(248,89,0)");
-        setFontColorById("Varo UV säteilyä!", "Red");
-    }
-    if (uvIndex >= 4.0 & uvIndex < 5.0) {
-        setBgColorById("rgb(248,135,0)");
-        setFontColorById("Suuri riski UV säteilystä", "Brown");
-    }
-    if (uvIndex >= 3.0 & uvIndex < 4.0) {
-        setBgColorById("rgb(248,182,0)");
-        setFontColorById("Riski UV säteilystä", "Orange");
-    }
-    if (uvIndex >= 2.0 & uvIndex < 3.0) {
-        setBgColorById("rgb(160,206,0)");
-        setFontColorById("Pieni riski UV säteilystä", "Gold");
-    }
-    if (uvIndex >= 1.0 & uvIndex < 2.0) {
-        setBgColorById("rgb(78,180,0)");
-        setFontColorById("Vähäinen riski UV säteilystä", "Cyan");
-    }
-    if (uvIndex >= 0 & uvIndex < 1.0) {
-        setBgColorById("rgb(190,190,190)");
-        setFontColorById("Ei vaaraa UV säteilystä", "blue");
-    }
-}
 
 function timeHour(timeValue) {
     // hour value
@@ -277,5 +226,53 @@ function UvWarning(uvIndex) {
     if (uvIndex >= 0 && uvIndex < 1.0) {
         //setBgColorById ("rgb(190,190,190)");
         return  "Ei vaaraa UV säteilystä";
+    }
+}
+
+// Set background color based on UVI index
+function setUviColor(uvIndex) {
+    if (uvIndex >= 10) {
+        setBgColorById("rgb(153,140,255)");
+        setFontColorById("PYSY POIS AURINGOSTA!", "Red");
+    }
+    if (uvIndex < 10 & uvIndex >= 9.0) {
+        setBgColorById("rgb(181,76,255)");
+        setFontColorById("PYSY POIS AURINGOSTA!", "Red");
+    }
+    if (uvIndex >= 8.0 & uvIndex < 9.0) {
+        setBgColorById("rgb(255,0,153)");
+        setFontColorById("PYSY POIS AURINGOSTA!", "Red");
+    }
+    if (uvIndex >= 7.0 & uvIndex < 8.0) {
+        setBgColorById("rgb(216,0,29)");
+        setFontColorById("5 min MAX AURINGOSSA!", "Red");
+    }
+    if (uvIndex >= 6.0 & uvIndex < 7.0) {
+        setBgColorById("rgb(232,44,14)");
+        setFontColorById("15 min MAX AURINGOSSA!", "Red");
+    }
+    if (uvIndex >= 5.0 & uvIndex < 6.0) {
+        setBgColorById("rgb(248,89,0)");
+        setFontColorById("Varo UV säteilyä!", "Red");
+    }
+    if (uvIndex >= 4.0 & uvIndex < 5.0) {
+        setBgColorById("rgb(248,135,0)");
+        setFontColorById("Suuri riski UV säteilystä", "Brown");
+    }
+    if (uvIndex >= 3.0 & uvIndex < 4.0) {
+        setBgColorById("rgb(248,182,0)");
+        setFontColorById("Riski UV säteilystä", "Orange");
+    }
+    if (uvIndex >= 2.0 & uvIndex < 3.0) {
+        setBgColorById("rgb(160,206,0)");
+        setFontColorById("Pieni riski UV säteilystä", "Gold");
+    }
+    if (uvIndex >= 1.0 & uvIndex < 2.0) {
+        setBgColorById("rgb(78,180,0)");
+        setFontColorById("Vähäinen riski UV säteilystä", "Cyan");
+    }
+    if (uvIndex >= 0 & uvIndex < 1.0) {
+        setBgColorById("rgb(190,190,190)");
+        setFontColorById("Ei vaaraa UV säteilystä", "blue");
     }
 }
